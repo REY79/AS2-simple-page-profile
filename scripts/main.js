a5c3fccbad18fd227dd41d1fd9faeef8e9dd1ca5
MR REINDOLF LAYE <56298997+REY79@users.noreply.github.com>
2020-07-01 21:21:59 +0100

    Delete main.js

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-/* SCRIPTS.JS */
-
-function changePic() {
-      document.getElementById("profilePic").src = "media/rey2.jpg";
-    }
-    
-    function normalPic() {
-      document.getElementById("profilePic") .src = "media/rey1.jpg";
-    }
