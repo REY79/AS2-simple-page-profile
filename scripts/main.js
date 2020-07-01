5ffa753e687d92a432cf3d7a4b59bebce7d484d5
MR REINDOLF LAYE <56298997+REY79@users.noreply.github.com>
2020-07-01 21:24:44 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function changePic() {
+      document.getElementById("profilePic").src = "media/rey2.jpeg";
+    }
+    
+    function normalPic() {
+      document.getElementById("profilePic") .src = "media/rey1.jpeg";
+    }
